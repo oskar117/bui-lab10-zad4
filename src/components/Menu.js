fb2b2dbe58cebb216c9496f4cb86fb659982ca0b
oskar117 <olek.konieczny@wp.pl>
2020-12-14 10:43:10 +0100

    add nav tag

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,13 +2,13 @@ import React from "react";
 
 export default function Menu() {
   return (
-    <>
+    <nav>
         <p>Navigation bar</p>
         <ul>
             <li>Home</li>
             <li>About</li>
             <li>Contact</li>
         </ul>
-    </>
+    </nav>
   );
 }
